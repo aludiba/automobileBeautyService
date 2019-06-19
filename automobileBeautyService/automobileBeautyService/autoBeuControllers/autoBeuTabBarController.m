33d1892a4350606c7878fbf7035774a29db299f9
aludiba <869922457@qq.com>
2019-06-19 22:54:36 +0800

    添加切换图片

--- automobileBeautyService/automobileBeautyService/autoBeuControllers/autoBeuTabBarController.m
+++ automobileBeautyService/automobileBeautyService/autoBeuControllers/autoBeuTabBarController.m
@@ -21,20 +21,26 @@
     self.autoBeuHomeVC = [[autoBeuHomeViewController alloc] init];
     UINavigationController *home = [[UINavigationController alloc] initWithRootViewController:self.autoBeuHomeVC];
     self.autoBeuHomeVC.tabBarItem.title = @"Home";
-    [self.autoBeuHomeVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor grayColor]} forState:UIControlStateSelected];
-    [self.autoBeuHomeVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor blackColor]} forState:UIControlStateNormal];
+    self.autoBeuHomeVC.tabBarItem.image = [[UIImage imageNamed:@"tab_home_default"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    self.autoBeuHomeVC.tabBarItem.selectedImage = [[UIImage imageNamed:@"tab_home_active"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    [self.autoBeuHomeVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor blackColor]} forState:UIControlStateSelected];
+    [self.autoBeuHomeVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor grayColor]} forState:UIControlStateNormal];
     
     self.autoBeuAddVC = [[autoBeuAddViewController alloc] init];
     UINavigationController *service = [[UINavigationController alloc] initWithRootViewController:self.autoBeuAddVC];
     self.autoBeuAddVC.tabBarItem.title = @"Add";
-    [self.autoBeuAddVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor grayColor]} forState:UIControlStateSelected];
-    [self.autoBeuAddVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor blackColor]} forState:UIControlStateNormal];
+    self.autoBeuAddVC.tabBarItem.image = [[UIImage imageNamed:@"tab_service_default"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    self.autoBeuAddVC.tabBarItem.selectedImage = [[UIImage imageNamed:@"tab_service_active"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    [self.autoBeuAddVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor blackColor]} forState:UIControlStateSelected];
+    [self.autoBeuAddVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor grayColor]} forState:UIControlStateNormal];
     
     self.autoBeuSearchVC = [[autoBeuSearchViewController alloc] init];
     UINavigationController *customer = [[UINavigationController alloc] initWithRootViewController:self.autoBeuSearchVC];
     self.autoBeuSearchVC.tabBarItem.title = @"Search";
-    [self.autoBeuSearchVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor grayColor]} forState:UIControlStateSelected];
-    [self.autoBeuSearchVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor blackColor]} forState:UIControlStateNormal];
+    self.autoBeuSearchVC.tabBarItem.image = [[UIImage imageNamed:@"tab_process_default"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    self.autoBeuSearchVC.tabBarItem.selectedImage = [[UIImage imageNamed:@"tab_process_active"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    [self.autoBeuSearchVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor blackColor]} forState:UIControlStateSelected];
+    [self.autoBeuSearchVC.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor grayColor]} forState:UIControlStateNormal];
     
     NSArray *arrControllers = [NSArray arrayWithObjects:home,service,customer,nil];
     self.viewControllers = arrControllers;
