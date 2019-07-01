d93870d2d95a04803cbe11a874f39641c38ce047
aludiba <869922457@qq.com>
2019-07-01 16:25:21 +0800

    添加IQKeyboardManager库

---
+++ smallTeamBuildingActivities/smallTeamBuildingActivities/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  smallTeamBuildingActivities
+//
+//  Created by bykj on 2019/7/1.
+//  Copyright © 2019 hgg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ smallTeamBuildingActivities/smallTeamBuildingActivities/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  smallTeamBuildingActivities
+//
+//  Created by bykj on 2019/7/1.
+//  Copyright © 2019 hgg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
