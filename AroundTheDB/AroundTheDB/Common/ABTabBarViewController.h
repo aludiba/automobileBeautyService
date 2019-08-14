9ec4dfccf101dccd1875ccaa1f2b4e4b2166ddd7
windy_hujiao <windy_hujiao@163.com>
2019-08-14 11:38:34 +0800

    新提交文件

---
+++ AroundTheDB/AroundTheDB/Common/ABTabBarViewController.h
@@ -0,0 +1,17 @@
+//
+//  ABTabBarViewController.h
+//  AroundTheDB
+//
+//  Created by bykj on 2019/8/14.
+//  Copyright © 2019 hgg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ABTabBarViewController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AroundTheDB/AroundTheDB/Common/ABTabBarViewController.m
@@ -0,0 +1,21 @@
+//
+//  ABTabBarViewController.m
+//  AroundTheDB
+//
+//  Created by bykj on 2019/8/14.
+//  Copyright © 2019 hgg. All rights reserved.
+//
+
+#import "ABTabBarViewController.h"
+
+@interface ABTabBarViewController ()
+
+@end
+
+@implementation ABTabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+@end
