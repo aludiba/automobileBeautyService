38ff3e017bf916cd34b2dc8510090bdd58c973d6
windy_hujiao <windy_hujiao@163.com>
2019-08-19 22:46:35 +0800

    暂存

--- AroundTheDB/AroundTheDB/Controllers/ABHistoryViewController.m
+++ AroundTheDB/AroundTheDB/Controllers/ABHistoryViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = ABH_Color(15, 18, 39, 1);
+    
 }
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
