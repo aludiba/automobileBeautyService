79a8ce53393023351405202f7029e444c370fbcf
windy_hujiao <windy_hujiao@163.com>
2019-08-27 15:58:30 +0800

    新建项目口袋记账

---
+++ PocketBooks/PocketBooks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PocketBooks
+//
+//  Created by bykj on 2019/8/27.
+//  Copyright © 2019 hbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PocketBooks/PocketBooks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PocketBooks
+//
+//  Created by bykj on 2019/8/27.
+//  Copyright © 2019 hbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
