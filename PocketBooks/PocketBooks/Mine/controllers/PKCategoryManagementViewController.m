24ec3c62992f3acb7d4693135e8cd9fe13813367
windy_hujiao <windy_hujiao@163.com>
2019-08-29 15:21:28 +0800

    类别管理初建

--- PocketBooks/PocketBooks/Mine/controllers/PKCategoryManagementViewController.m
+++ PocketBooks/PocketBooks/Mine/controllers/PKCategoryManagementViewController.m
@@ -8,14 +8,52 @@
 
 #import "PKCategoryManagementViewController.h"
 
-@interface PKCategoryManagementViewController ()
-
+@interface PKCategoryManagementViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
+@property(nonatomic, strong)NSMutableArray *selectArray;
+@property(nonatomic, strong)NSMutableArray *dataArray;
+@property(nonatomic, strong)UICollectionView *collectionView;
 @end
 
 @implementation PKCategoryManagementViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+#pragma mark - 属性懒加载
+- (NSMutableArray *)selectArray{
+    if (!_selectArray) {
+        _selectArray = [[NSMutableArray alloc] init];
+    }
+    return _selectArray;
+}
+- (NSMutableArray *)dataArray{
+    if (!_dataArray) {
+        _dataArray = [[NSMutableArray alloc] init];
+    }
+    return _dataArray;
+}
+- (UICollectionView *)collectionView{
+    if (!_collectionView) {
+        // 创建FlowLayout
+        UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+        // 垂直方向滑动
+        flowLayout.scrollDirection = UICollectionViewScrollDirectionVertical;
+        // 创建collectionView
+        CGRect frame = CGRectMake(0, PKHeightNavBar, PKWIDTH, PKHEIGHT);
+        _collectionView = [[UICollectionView alloc] initWithFrame:frame collectionViewLayout:flowLayout];
+        // 设置代理
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+        // 其他属性
+        _collectionView.backgroundColor = [UIColor clearColor];
+        _collectionView.showsVerticalScrollIndicator = NO;          // 隐藏垂直方向滚动条
+//        // 注册cell
+//        [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"MyCell"];
+//        // 注册Header
+//        [_collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"MyHeader"];
+//        // 注册Footer
+//        [_collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"MyFooter"];
+    }
+    return _collectionView;
 }
 @end
