641c1b7ad6b6e007c5997d902dffadc695decb3a
windy_hujiao <windy_hujiao@163.com>
2019-09-05 16:43:47 +0800

    预算展示

--- PocketBooks/PocketBooks/Budget/controllers/PKBudgetViewController.m
+++ PocketBooks/PocketBooks/Budget/controllers/PKBudgetViewController.m
@@ -8,8 +8,14 @@
 
 #import "PKBudgetViewController.h"
 #import "PKAddBudgetViewController.h"
-@interface PKBudgetViewController ()
+#import "PKBudgetTableViewCell.h"
+#import "PKBudgetModel.h"
+
+@interface PKBudgetViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property(nonatomic, strong)NSMutableArray *dataArray;
+@property(nonatomic, strong)UITableView *mainTable;
 @property(nonatomic, strong)UIButton *addButton;
+@property(nonatomic, copy)NSString *nowTimeStampString;
 @end
 
 @implementation PKBudgetViewController
@@ -18,7 +24,13 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = PKH_Color(244, 245, 246, 1);
-    self.title = NSLocalizedString(@"预算", nil);
+    self.title = NSLocalizedString(@"当天预算", nil);
+    NSDate *nowDate = [[NSDate alloc] init];
+    self.nowTimeStampString = [PKUIUtilities PKformattedTimeStringWithDate:nowDate format:@"yyyyMM"];
+}
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self loadInitData];
 }
 - (void)PK_setupNavigationItems{
     [super PK_setupNavigationItems];
@@ -28,12 +40,121 @@
     UIBarButtonItem *addButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.addButton];
     self.navigationItem.rightBarButtonItem = addButtonItem;
 }
+- (void)loadInitData{
+    [self.dataArray removeAllObjects];
+
+    BmobQuery *bquery = [BmobQuery queryWithClassName:@"PKBudget"];
+    BmobUser *author = [BmobUser currentUser];
+    [bquery whereKey:@"author" equalTo:author];
+    //查找GameScore表的数据
+    __weak typeof(self) weakSelf = self;
+    [bquery findObjectsInBackgroundWithBlock:^(NSArray *array, NSError *error) {
+        if (error) {
+            [self.mainTable.mj_header endRefreshing];
+            [MBProgressHUD PKshowReminderText:NSLocalizedString(@"暂无数据，请点击加号添加", nil)];
+        }else{
+            [self.mainTable.mj_header endRefreshing];
+            if (array.count) {
+                BmobObject *obj = [array lastObject];
+                NSDate *date = [obj objectForKey:@"date"];
+                NSString *dateString = [PKUIUtilities PKformattedTimeStringWithDate:date format:@"yyyy-MM-dd"];
+                NSDate *nowDate = [[NSDate alloc] init];
+                NSString *nowDateString = [PKUIUtilities PKformattedTimeStringWithDate:nowDate format:@"yyyy-MM-dd"];
+                if ([dateString isEqualToString:nowDateString]) {
+                    NSArray *dataArray  = [obj objectForKey:@"data"];
+                    for (int i = 0;i < dataArray.count; i++) {
+                        NSDictionary *dic = dataArray[i];
+                        PKBudgetModel *model = [[PKBudgetModel alloc] init];
+                        model.content = [dic objectForKey:@"content"];
+                        model.unit = [dic objectForKey:@"unit"];
+                        model.code = [[dic objectForKey:@"code"] integerValue];
+                        model.limits = [dic objectForKey:@"limits"];
+                        switch (model.code) {
+                            case 000:
+                                model.title = NSLocalizedString(@"服饰", nil);
+                                break;
+                            case 001:
+                                model.title = NSLocalizedString(@"食品", nil);
+                                break;
+                            case 002:
+                                model.title = NSLocalizedString(@"房屋", nil);
+                                break;
+                            case 003:
+                                model.title = NSLocalizedString(@"交通", nil);
+                                break;
+                            case 004:
+                                model.title = NSLocalizedString(@"健康", nil);
+                                break;
+                            case 005:
+                                model.title = NSLocalizedString(@"空闲", nil);
+                                break;
+                            case 006:
+                                model.title = NSLocalizedString(@"网费", nil);
+                                break;
+                            case 007:
+                                model.title = NSLocalizedString(@"手机", nil);
+                                break;
+                            case 071:
+                                model.title = NSLocalizedString(@"水费", nil);
+                                break;
+                            case 072:
+                                model.title = NSLocalizedString(@"电费", nil);
+                                break;
+                            case 010:
+                                model.title = NSLocalizedString(@"取暖费", nil);
+                                break;
+                            case 011:
+                                model.title = NSLocalizedString(@"物业", nil);
+                                break;
+                            case 012:
+                                model.title = NSLocalizedString(@"车位", nil);
+                                break;
+                            case 013:
+                                model.title = NSLocalizedString(@"装修", nil);
+                                break;
+                            case 014:
+                                model.title = NSLocalizedString(@"家电", nil);
+                                break;
+                            case 015:
+                                model.title = NSLocalizedString(@"服务", nil);
+                                break;
+                            case 016:
+                                model.title = NSLocalizedString(@"家居", nil);
+                                break;
+                            case 017:
+                                model.title = NSLocalizedString(@"其它", nil);
+                                break;
+                            default:
+                                break;
+                        }
+                        [weakSelf.dataArray addObject:model];
+                    }
+                    [weakSelf.mainTable reloadData];
+                }else{
+                    [MBProgressHUD PKshowReminderText:NSLocalizedString(@"暂无数据，请点击加号添加", nil)];
+                }
+            }else{
+                [MBProgressHUD PKshowReminderText:NSLocalizedString(@"暂无数据，请点击加号添加", nil)];
+            }
+        }
+    }];
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.dataArray.count;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    PKBudgetModel *model = self.dataArray[indexPath.row];
+    PKBudgetTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PKBudgetTableViewCell" forIndexPath:indexPath];
+    cell.model = model;
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
+}
 - (void)addBtnClick:(UIButton *)sender{
-    NSLog(@"添加新预算~~~~~~");
     PKAddBudgetViewController *vc = [[PKAddBudgetViewController alloc] init];
     vc.hidesBottomBarWhenPushed = YES;
     [self.navigationController pushViewController:vc animated:YES];
 }
+
 #pragma mark - 属性懒加载
 - (UIButton *)addButton{
     if (!_addButton) {
@@ -43,5 +164,36 @@
         [_addButton addTarget:self action:@selector(addBtnClick:) forControlEvents:UIControlEventTouchUpInside];
     }
     return _addButton;
+}
+- (NSMutableArray *)dataArray{
+    if (!_dataArray) {
+        _dataArray = [[NSMutableArray alloc] init];
+    }
+    return _dataArray;
+}
+- (UITableView *)mainTable{
+    if (!_mainTable) {
+        _mainTable = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _mainTable.delegate = self;
+        _mainTable.dataSource = self;
+        _mainTable.backgroundColor = PKH_Color(244, 245, 246, 1);
+        _mainTable.separatorStyle = UITableViewCellSeparatorStyleNone;
+        _mainTable.delegate = self;
+        _mainTable.dataSource = self;
+        _mainTable.rowHeight = UITableViewAutomaticDimension;
+        _mainTable.estimatedRowHeight = 55.0f;
+        _mainTable.tableHeaderView = [[UIView alloc] init];
+        _mainTable.tableFooterView = [[UIView alloc] init];
+        [_mainTable registerClass:[PKBudgetTableViewCell class] forCellReuseIdentifier:@"PKBudgetTableViewCell"];
+        _mainTable.mj_header = [MJRefreshNormalHeader headerWithRefreshingTarget:self refreshingAction:@selector(loadInitData)];
+        [self.view addSubview:_mainTable];
+        [_mainTable mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.mas_topLayoutGuideBottom).offset(10);
+            make.leading.equalTo(self.view);
+            make.trailing.equalTo(self.view);
+            make.bottom.equalTo(self.view).offset(-PKHeightTPKBar);
+        }];
+    }
+    return _mainTable;
 }
 @end
--- PocketBooks/PocketBooks/Budget/views/PKBudgetTableViewCell.m
+++ PocketBooks/PocketBooks/Budget/views/PKBudgetTableViewCell.m
@@ -20,6 +20,7 @@
 @implementation PKBudgetTableViewCell
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
     if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        self.contentView.backgroundColor = PKH_Color(244, 245, 246, 1);
         [self.contentView addSubview:self.titleLbl];
         [self.contentView addSubview:self.contentLbl];
         [self.contentView addSubview:self.limitsLbl];
@@ -28,19 +29,58 @@
         
         [self.titleLbl mas_makeConstraints:^(MASConstraintMaker *make) {
             make.leading.equalTo(self.contentView).offset(30);
-            make.top.equalTo(self.contentView).offset(60);
+            make.top.equalTo(self.contentView).offset(30);
             make.width.mas_equalTo(80);
             make.height.mas_equalTo(40);
         }];
+        [self.contentLbl mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.leading.equalTo(self.titleLbl.mas_trailing);
+            make.centerY.equalTo(self.titleLbl);
+            make.height.mas_equalTo(20);
+            make.width.mas_equalTo(PKWIDTH * 0.5 - 110);
+        }];
+        [self.limitsLbl mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.trailing.equalTo(self.contentView).offset(-30);
+            make.centerY.equalTo(self.titleLbl);
+            make.height.mas_equalTo(20);
+             make.width.mas_equalTo(PKWIDTH * 0.5 - 30);
+        }];
+        [self.backView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.leading.equalTo(self.contentView).offset(30);
+            make.top.equalTo(self.titleLbl.mas_bottom).offset(5);
+            make.trailing.equalTo(self.contentView).offset(-30);
+            make.height.mas_equalTo(10);
+            make.bottom.equalTo(self.contentView);
+        }];
+        [self.contentV mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.leading.equalTo(self.backView);
+            make.top.equalTo(self.backView);
+            make.width.mas_equalTo(PKWIDTH - 60);
+            make.height.mas_equalTo(10);
+        }];
     }
     return self;
+}
+- (void)setModel:(PKBudgetModel *)model{
+    _model = model;
+    self.titleLbl.text = _model.title;
+    self.contentLbl.text = [NSString stringWithFormat:@"%@%@",_model.content,_model.unit];
+    self.limitsLbl.text = [NSString stringWithFormat:@"%@:%@%@",NSLocalizedString(@"限额", nil),_model.limits,_model.unit];
+    CGFloat contentNum = [_model.content floatValue];
+    CGFloat limitsNum = [_model.limits floatValue];
+    CGFloat contentVWidth = (contentNum / limitsNum) * (PKWIDTH - 60);
+    [self.contentV mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(contentVWidth);
+    }];
+    [self.contentV layoutIfNeeded];
+    [self.contentView layoutSubviews];
 }
 #pragma mark - 属性懒加载
 - (UILabel *)titleLbl{
     if (!_titleLbl) {
         _titleLbl = [[UILabel alloc] init];
-        _titleLbl.textColor = [UIColor redColor];
-        _titleLbl.font = [UIFont systemFontOfSize:20];
+        _titleLbl.textColor = [UIColor blueColor];
+        _titleLbl.font = [UIFont boldSystemFontOfSize:20];
         _titleLbl.numberOfLines = 0;
     }
     return _titleLbl;
@@ -48,8 +88,8 @@
 - (UILabel *)contentLbl{
     if (!_contentLbl) {
         _contentLbl = [[UILabel alloc] init];
-        _contentLbl.textColor = [UIColor redColor];
-        _contentLbl.font = [UIFont systemFontOfSize:15];
+        _contentLbl.textColor = [UIColor purpleColor];
+        _contentLbl.font = [UIFont systemFontOfSize:17];
         _contentLbl.numberOfLines = 0;
     }
     return _contentLbl;
@@ -58,7 +98,8 @@
     if (!_limitsLbl) {
         _limitsLbl = [[UILabel alloc] init];
         _limitsLbl.textColor = [UIColor redColor];
-        _limitsLbl.font = [UIFont systemFontOfSize:16];
+        _limitsLbl.font = [UIFont systemFontOfSize:18];
+        _limitsLbl.textAlignment = NSTextAlignmentRight;
         _limitsLbl.numberOfLines = 0;
     }
     return _limitsLbl;
@@ -66,7 +107,9 @@
 - (UIView *)backView{
     if (!_backView) {
         _backView = [[UIView alloc] init];
-        _backView.backgroundColor = [UIColor grayColor];
+        _backView.backgroundColor = PKH_Color(230, 230, 230, 1);
+        _backView.layer.cornerRadius = 2;
+        _backView.layer.masksToBounds = YES;
     }
     return _backView;
 }
