8100dec1b410386a9228f009ce478265d2a51de2
windy_hujiao <windy_hujiao@163.com>
2019-09-06 16:47:36 +0800

    新建历史记录header文件

---
+++ PocketBooks/PocketBooks/BillHistory/views/PKBillHistoryHeaderView.h
@@ -0,0 +1,17 @@
+//
+//  PKBillHistoryHeaderView.h
+//  PocketBooks
+//
+//  Created by bykj on 2019/9/6.
+//  Copyright © 2019 hbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PKBillHistoryHeaderView : UITableViewHeaderFooterView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PocketBooks/PocketBooks/BillHistory/views/PKBillHistoryHeaderView.m
@@ -0,0 +1,15 @@
+//
+//  PKBillHistoryHeaderView.m
+//  PocketBooks
+//
+//  Created by bykj on 2019/9/6.
+//  Copyright © 2019 hbc. All rights reserved.
+//
+
+#import "PKBillHistoryHeaderView.h"
+@interface PKBillHistoryHeaderView()
+
+@end
+@implementation PKBillHistoryHeaderView
+
+@end
