f0e12e4c10c59db1be0af1b197e4e6771c1dbf85
windy_hujiao <windy_hujiao@163.com>
2019-10-06 09:30:55 +0800

    新项目酒醒

---
+++ wineAwake/wineAwake/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  wineAwake
+//
+//  Created by bykj on 2019/10/6.
+//  Copyright © 2019 hbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ wineAwake/wineAwake/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  wineAwake
+//
+//  Created by bykj on 2019/10/6.
+//  Copyright © 2019 hbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
