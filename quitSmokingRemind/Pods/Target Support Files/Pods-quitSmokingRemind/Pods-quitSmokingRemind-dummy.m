2456c3d45a577a5670d0bb39233229a35eb54e52
windy_hujiao <windy_hujiao@163.com>
2019-10-14 09:37:17 +0800

    戒烟打卡计划添加Masonry

---
+++ quitSmokingRemind/Pods/Target Support Files/Pods-quitSmokingRemind/Pods-quitSmokingRemind-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_quitSmokingRemind : NSObject
+@end
+@implementation PodsDummy_Pods_quitSmokingRemind
+@end
---
+++ quitSmokingRemind/quitSmokingRemind/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  quitSmokingRemind
+//
+//  Created by bykj on 2019/10/14.
+//  Copyright © 2019 hbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
