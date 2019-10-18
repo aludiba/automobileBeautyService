f6cda4ca2d2de2b177fca09769db3c841d9f38ab
windy_hujiao <windy_hujiao@163.com>
2019-10-18 09:13:05 +0800

    新建项目sodaReminder

---
+++ sodaReminder/sodaReminder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sodaReminder
+//
+//  Created by bykj on 2019/10/18.
+//  Copyright © 2019 hbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ sodaReminder/sodaReminder/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  sodaReminder
+//
+//  Created by bykj on 2019/10/18.
+//  Copyright © 2019 hbc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
