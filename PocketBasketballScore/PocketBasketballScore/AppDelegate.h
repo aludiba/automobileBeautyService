07626cdaaed3b0dafe25c2bc241f97f3f93f8f45
windy_hujiao <windy_hujiao@163.com>
2019-10-22 17:32:29 +0800

    新建项目掌上篮球计分并添加'Masonry'库

---
+++ PocketBasketballScore/PocketBasketballScore/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PocketBasketballScore
+//
+//  Created by bykj on 2019/10/22.
+//  Copyright © 2019 hbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PocketBasketballScore/PocketBasketballScore/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  PocketBasketballScore
+//
+//  Created by bykj on 2019/10/22.
+//  Copyright © 2019 hbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
