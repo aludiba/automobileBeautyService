9025bf2c860f9eef51ea791dcb6bd87240a3f857
windy_hujiao <windy_hujiao@163.com>
2019-11-18 11:09:02 +0800

    新建项目篮球球探

---
+++ PocketBasketballScout/PocketBasketballScout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PocketBasketballScout
+//
+//  Created by bykj on 2019/11/18.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
