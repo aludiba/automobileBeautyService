40b9099da850524922a3f6ecad9befc014e68caf
windy_hujiao <windy_hujiao@163.com>
2019-12-04 17:59:44 +0800

    添加Masonry库

---
+++ ConvenientFootballScoreboard/ConvenientFootballScoreboard/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ConvenientFootballScoreboard
+//
+//  Created by bykj on 2019/12/4.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
---
+++ ConvenientFootballScoreboard/ConvenientFootballScoreboard/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  ConvenientFootballScoreboard
+//
+//  Created by bykj on 2019/12/4.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+@end
