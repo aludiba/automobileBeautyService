3b7e8105e03f4af7682fd67d5896cad53ef2dc5e
windy_hujiao <windy_hujiao@163.com>
2019-12-27 14:21:24 +0800

    新建项目口袋里的幸运数字

---
+++ PocketLuckyNumbers/PocketLuckyNumbers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PocketLuckyNumbers
+//
+//  Created by bykj on 2019/12/27.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
