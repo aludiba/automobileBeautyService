692dbc934f584a410770b40f4fe1dfdfb933902e
windy_hujiao <windy_hujiao@163.com>
2020-04-24 22:28:38 +0800

    ccc

---
+++ YourNiceWardrobe/YourNiceWardrobe/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  YourNiceWardrobe
+//
+//  Created by 褚红彪 on 2020/4/24.
+//  Copyright © 2020 hb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
