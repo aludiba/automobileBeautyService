00c7d27aadc4a71b0e989b71fe34ae2233096ba0
windy_hujiao <windy_hujiao@163.com>
2020-04-26 18:33:05 +0800

    提交新功能

--- YourNiceWardrobe/YourNiceWardrobe/BCCommon/BCGlobal.h
+++ YourNiceWardrobe/YourNiceWardrobe/BCCommon/BCGlobal.h
@@ -1,13 +1,13 @@
 //
-//  BCglobBC.h
+//  ALglobal.h
 //  Simple Diary
 //
 //  Created by bykj on 2019/7/20.
-//  Copyright © 2019 hgg. BCl rights reserved.
+//  Copyright © 2019 hgg. All rights reserved.
 //
 
-#ifndef BCglobBC_h
-#define BCglobBC_h
+#ifndef BCglobal_h
+#define BCglobal_h
 #import <Masonry/Masonry.h>
 #import <MJRefresh/MJRefresh.h>
 #import <MBProgressHUD/MBProgressHUD.h>
@@ -21,29 +21,29 @@
 #import "NSDictionary+log.h"
 #import <AVOSCloud/AVOSCloud.h>
 //多语言调用
-#define NSLocBCizedString(key, comment) [[NSBundle mainBundle] locBCizedStringForKey:(key) vBCue:@"" table:nil]
+#define NSLocalizedString(key, comment) [[NSBundle mainBundle] localizedStringForKey:(key) value:@"" table:nil]
 /**颜色*/
-#define BCH_Color(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 BCpha:a]
+#define BCH_Color(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:a]
 #define BCKeyWindow [UIApplication sharedApplication].keyWindow
 #define BCWIDTH [UIScreen mainScreen].bounds.size.width
 #define BCHEIGHT [UIScreen mainScreen].bounds.size.height
 //判断是否是ipad
 #define BCisPad ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
-#define BCkiPhone4 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEquBCToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
+#define BCkiPhone4 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
 //判断iPhone5系列
-#define BCkiPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEquBCToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
+#define BCkiPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
 //判断iPhone6系列
-#define BCkiPhone6 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEquBCToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
+#define BCkiPhone6 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
 //判断iphone6+系列
-#define BCkiPhone6Plus ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEquBCToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
+#define BCkiPhone6Plus ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
 //判断iPhoneX
-#define BCIS_IPHONE_X ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEquBCToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
+#define BCIS_IPHONE_X ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
 //判断iPHoneXr
-#define BCIS_IPHONE_Xr ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEquBCToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
+#define BCIS_IPHONE_Xr ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
 //判断iPhoneXs
-#define BCIS_IPHONE_Xs ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEquBCToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
+#define BCIS_IPHONE_Xs ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
 //判断iPhoneXs Max
-#define BCIS_IPHONE_Xs_Max ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEquBCToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
+#define BCIS_IPHONE_Xs_Max ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) && !BCisPad : NO)
 //iPhoneX系列
 #define BCISIPHONEX ((BCIS_IPHONE_X==YES || BCIS_IPHONE_Xr ==YES || BCIS_IPHONE_Xs== YES || BCIS_IPHONE_Xs_Max== YES) ? YES : NO)
 #define BCHeightStatusBar ((BCIS_IPHONE_X==YES || BCIS_IPHONE_Xr ==YES || BCIS_IPHONE_Xs== YES || BCIS_IPHONE_Xs_Max== YES) ? 44.0 : 20.0)
@@ -56,4 +56,4 @@ typedef NS_ENUM(NSInteger, BCCargoType) {
     BCCargoTypeWaitDelivery,
     BCCargoTypeCompleted,
 };
-#endif /* BCglobBC_h */
+#endif /* ALglobal_h */
--- YourNiceWardrobe/YourNiceWardrobe/BCWardrobe/BCControllers/BCWardrobeViewController.m
+++ YourNiceWardrobe/YourNiceWardrobe/BCWardrobe/BCControllers/BCWardrobeViewController.m
@@ -8,8 +8,12 @@
 
 #import "BCWardrobeViewController.h"
 
-@interface BCWardrobeViewController ()
-
+@interface BCWardrobeViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property(nonatomic, strong)NSMutableArray *BCDataArray;
+@property(nonatomic, strong)UIView *BCBackView;
+@property(nonatomic, strong)UIImageView *BCcoatImgView;//外套
+@property(nonatomic, strong)UIButton *BCaddButton;
+@property(nonatomic, strong)UITableView *BCmainTable;//列表
 @end
 
 @implementation BCWardrobeViewController
@@ -18,16 +22,90 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.title = @"衣柜";
+    [self BCsetcontentView];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)BC_setupNavigationItems{
+    [super BC_setupNavigationItems];
+    UIBarButtonItem *BCaddItem = [[UIBarButtonItem alloc] initWithCustomView:self.BCaddButton];
+    self.navigationItem.rightBarButtonItem = BCaddItem;
 }
-*/
-
+- (void)BCsetcontentView{
+    self.view.backgroundColor = BCH_Color(242, 242, 242, 1);
+    [self.view addSubview:self.BCBackView];
+    [self.BCBackView mas_makeConstraints:^(MASConstraintMaker *make) {
+       make.top.equalTo(self.view).offset(BCHeightNavBar);
+        make.leading.equalTo(self.view);
+        make.trailing.equalTo(self.view);
+        make.height.mas_equalTo(140);
+    }];
+    [self.BCBackView addSubview:self.BCcoatImgView];
+    [self.BCcoatImgView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.BCBackView).offset(20);
+        make.centerX.equalTo(self.BCBackView);
+        make.width.mas_equalTo(100);
+        make.height.mas_equalTo(100);
+    }];
+    [self.view addSubview:self.BCmainTable];
+    [self.BCmainTable mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.BCBackView.mas_bottom);
+        make.leading.equalTo(self.view);
+        make.trailing.equalTo(self.view);
+        make.bottom.equalTo(self.view);
+    }];
+}
+#pragma mark - UITableViewDelegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.BCDataArray.count;
+}
+#pragma mark - actions
+- (void)BCaddAction:(UIButton *)sender{
+    UIAlertController *BCalertVC = [UIAlertController alertControllerWithTitle:@"提醒" message:@"请先登录" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *BCcancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+    UIAlertAction *BCsureAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    [BCalertVC addAction:BCcancelAction];
+    [BCalertVC addAction:BCsureAction];
+    [self presentViewController:BCalertVC animated:YES completion:nil];
+}
+#pragma mark - 属性懒加载
+- (UIView *)BCBackView{
+    if (!_BCBackView) {
+        _BCBackView = [[UIView alloc] init];
+        _BCBackView.backgroundColor = BCH_Color(246, 246, 246, 1);
+    }
+    return _BCBackView;
+}
+- (UIImageView *)BCcoatImgView{
+    if (!_BCcoatImgView) {
+        _BCcoatImgView = [[UIImageView alloc] init];
+        _BCcoatImgView.image = [UIImage imageNamed:@"coat"];
+    }
+    return _BCcoatImgView;
+}
+- (UIButton *)BCaddButton{
+    if (!_BCaddButton) {
+        _BCaddButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
+        [_BCaddButton setImage:[UIImage imageNamed:@"BC_add"] forState:UIControlStateNormal];
+        [_BCaddButton addTarget:self action:@selector(BCaddAction:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _BCaddButton;
+}
+- (NSMutableArray *)BCDataArray{
+    if (!_BCDataArray) {
+        _BCDataArray = [[NSMutableArray alloc] init];
+    }
+    return _BCDataArray;
+}
+- (UITableView *)BCmainTable{
+    if (!_BCmainTable) {
+        _BCmainTable = [[UITableView alloc] init];
+        _BCmainTable.rowHeight = UITableViewAutomaticDimension;
+        _BCmainTable.estimatedRowHeight = 48.0f;
+        _BCmainTable.dataSource = self;
+        _BCmainTable.delegate = self;
+        _BCmainTable.separatorStyle = UITableViewCellSeparatorStyleNone;
+    }
+    return _BCmainTable;
+}
 @end
