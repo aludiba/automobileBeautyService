6566898b65ee4f2fabcfa91dfff49e44f9186efd
user <windy_zhonganchu@163.com>
2020-06-09 11:31:14 +0800

    新建美好省会与良好习惯app

---
+++ GoodLifeAndGoodHabits/GoodLifeAndGoodHabits/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoodLifeAndGoodHabits
+//
+//  Created by user on 2020/6/9.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
