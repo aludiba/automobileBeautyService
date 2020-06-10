a2ad9c220ea2226aa2fb0884f5c431825a67a4cc
user <windy_zhonganchu@163.com>
2020-06-10 13:07:59 +0800

    新增首页头视图

--- GoodLifeAndGoodHabits/GoodLifeAndGoodHabits/JBHome/JBControllers/JBHomeViewController.m
+++ GoodLifeAndGoodHabits/GoodLifeAndGoodHabits/JBHome/JBControllers/JBHomeViewController.m
@@ -9,7 +9,7 @@
 #import "JBHomeViewController.h"
 
 @interface JBHomeViewController ()
-
+@property(nonatomic, strong)UIImageView *JBHeadImgView;//头图片
 @end
 
 @implementation JBHomeViewController
@@ -17,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = NSLocalizedString(@"首页", nil);
+    [self JBsetContentView];
 }
-
+- (void)JBsetContentView{
+    [self.view addSubview:self.JBHeadImgView];
+    [self.JBHeadImgView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view);
+        make.leading.equalTo(self.view);
+        make.trailing.equalTo(self.view);
+        make.height.mas_equalTo(200);
+    }];
+}
+#pragma mark - 属性懒加载
+- (UIImageView *)JBHeadImgView{
+    if (!_JBHeadImgView) {
+        _JBHeadImgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"JBHome_changheluori"]];
+    }
+    return _JBHeadImgView;
+}
 @end
