ccb3899eac97cba8fa66575c21f94ee4e0171fbb
aludiba <869922457@qq.com>
2020-07-06 21:13:38 +0800

    宏体育

---
+++ MacroSports/MacroSports/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MacroSports
+//
+//  Created by user on 2020/6/30.
+//  Copyright © 2020 macro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic)UIWindow *LBwindow;
+@end
+
---
+++ MacroSports/MacroSports/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MacroSports
+//
+//  Created by user on 2020/6/30.
+//  Copyright © 2020 macro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
